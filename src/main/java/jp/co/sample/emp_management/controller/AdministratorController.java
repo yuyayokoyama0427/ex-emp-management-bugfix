60686caf4127471430f79ace1bb1c425e496d953
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-24 12:29:36 +0900

    (1-1)ログイン画面への遷移へ改修

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -77,7 +77,7 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-		return "employee/list";
+		return "administrator/login";
 	}
 
 	/////////////////////////////////////////////////////
