a43a71ca890dbb245bbe8ffe0233cb71b0d85c71
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-25 10:13:36 +0900

    (5-1)中級 SQL Injectionの対策

--- src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
@@ -58,8 +58,8 @@ public class AdministratorRepository {
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
 	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address= '" + mailAddress + "' and password='" + password + "'";
-		SqlParameterSource param = new MapSqlParameterSource();
+		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress and password=:password";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
 			return null;
