97ec4a3ef7b8ed3f7d2cfeb255be633d0590e40f
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-27 16:51:11 +0900

    formの作成完了

--- src/main/java/jp/co/sample/emp_management/form/FindEmployee.java
+++ src/main/java/jp/co/sample/emp_management/form/FindEmployee.java
@@ -1,5 +1,28 @@
 package jp.co.sample.emp_management.form;
 
+/**
+ * 従業員曖昧検索をするフォーム.
+ * 
+ * @author yuyayokoyama
+ *
+ */
 public class FindEmployee {
+	/** 従業員名 */
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "FindEmployee [name=" + name + "]";
+	}
+	
+	
 
 }
